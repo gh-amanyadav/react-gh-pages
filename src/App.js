afe3afae14dc76e5a1c39b9e644fb7cb078e4411
Aman kumar <amankumar99347@gmail.com>
2022-08-28 17:48:28 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi <code>This is a workshop</code> on deploying a react application to github.
         </p>
         <a
           className="App-link"
